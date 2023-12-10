2cf494bab80582d58cd0c0703d2a9d23140cfec8
Pinja Rontu <145121433+pinskii@users.noreply.github.com>
2023-12-10 21:32:58 +0200

    Change null and undefined to expect '' as in the examples

--- tests/toString.test.js
+++ tests/toString.test.js
@@ -2,7 +2,8 @@ import toString from '../files/toString.js';
 
 describe('toString function', () => {
   test('converts various values to strings', () => {
-    expect(toString(null)).toBe("null");
+    expect(toString(null)).toBe('');
+    expect(toString(undefined)).toBe('');
     expect(toString(-0)).toBe('-0');
     expect(toString([1, 2, 3])).toBe('1,2,3');
     expect(toString({ key: 'value' })).toBe('[object Object]');
