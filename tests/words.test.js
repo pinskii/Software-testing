aa8c80495ecad4e8851fcb196385e225446313c7
Pinja Rontu <145121433+pinskii@users.noreply.github.com>
2023-12-10 21:48:45 +0200

    Update words.test.js to have a string without commas

--- tests/words.test.js
+++ tests/words.test.js
@@ -4,5 +4,6 @@ describe('words function', () => {
   test('splits string into an array of words', () => {
     expect(words('fred, barney, & pebbles')).toEqual(['fred', 'barney', 'pebbles']);
     expect(words('fred, barney, & pebbles', /[^, ]+/g)).toEqual(['fred', 'barney', '&', 'pebbles']);
+    expect(words('fred barney & pebbles')).toEqual(['fred', 'barney', 'pebbles']);
   });
 });
