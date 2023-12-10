7957ecd573bd812e73bca2bfaa715fcbec840f59
Pinja Rontu <145121433+pinskii@users.noreply.github.com>
2023-12-10 20:52:31 +0200

    Update defaultTo.test.js

--- tests/defaultTo.test.js
+++ tests/defaultTo.test.js
@@ -1,18 +1,15 @@
 import defaultTo from '../files/defaultTo.js';
 
 describe('defaultTo function', () => {
-  test('returns default value if input is null or undefined', () => {
+  test('returns default value if input is null or undefined or NaN', () => {
     expect(defaultTo(null, 10)).toBe(10);
     expect(defaultTo(undefined, 10)).toBe(10);
+    expect(defaultTo(NaN, 10)).toBe(10);
   });
 
   test('does not return default value if input is not null or undefined', () => {
     expect(defaultTo(0, 10)).toBe(0);
     expect(defaultTo('abc', 10)).toBe('abc');
     expect(defaultTo(false, 10)).toBe(false);
-  });
-
-  test('returns the input value if it is NaN', () => {
-    expect(defaultTo(NaN, 10)).toBe(NaN);
   });
 });
